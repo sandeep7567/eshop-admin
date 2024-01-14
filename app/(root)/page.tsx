20d30653c8f8df9f31b5afe9b6bc0a8facc33b85
sandeep7567 <sandeepthakur7567@gmail.com>
2024-01-14 09:59:16 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button variant={"sky"} size={"lg"}>
+        Hello Dashboard
+      </Button>
+    </div>
+  );
+}
